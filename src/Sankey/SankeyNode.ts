d88439727f4332a7d6902fa50686c11533fa0282
Daniel <danielimanihalter@gmail.com>
2025-08-30 15:04:54 -0400

    Make the title of the node config modal include the recipe name.

--- src/Sankey/SankeyNode.ts
+++ src/Sankey/SankeyNode.ts
@@ -337,7 +337,7 @@ export class SankeyNode extends EventTarget
 
         let openConfigurator = (event: Event) =>
         {
-            configurator.openConfigurationWindow(this.machinesAmount, this.overclockRatio);
+            configurator.openConfigurationWindow(recipe.displayName, this.machinesAmount, this.overclockRatio);
             event.stopPropagation();
         };
 
